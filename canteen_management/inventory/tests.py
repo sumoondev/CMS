b7a5a66c1a8f6c87971b54a4cf9ffaa6727946fd
Suhan Khadka <khsuhan100@gmail.com>
2026-03-09 23:45:51 +0545

    fix: Update image paths to use dynamic URLs

--- canteen_management/inventory/tests.py
+++ canteen_management/inventory/tests.py
@@ -1,5 +1,9 @@
+from django.conf import settings
 from django.test import TestCase, override_settings
 from django.urls import reverse
+
+from accounts.models import CustomUser
+from inventory.models import Inventory
 
 
 @override_settings(ALLOWED_HOSTS=['testserver', 'localhost'])
@@ -8,3 +12,45 @@ class InventoryAuthRedirectTests(TestCase):
         response = self.client.get(reverse('inventory_list'))
 
         self.assertRedirects(response, '/login/?next=/menu/')
+
+
+@override_settings(ALLOWED_HOSTS=['testserver', 'localhost'])
+class InventoryImageRenderingTests(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username='menu_user',
+            password='testpass123',
+            user_code='55555',
+            role='student',
+        )
+        self.client.login(username='menu_user', password='testpass123')
+
+    def test_media_root_is_separate_from_static_files(self):
+        self.assertNotEqual(str(settings.MEDIA_ROOT), str(settings.STATICFILES_DIRS[0]))
+
+    def test_menu_uses_media_url_for_uploaded_image(self):
+        Inventory.objects.create(
+            item_name='Tea',
+            category='beverages',
+            price='25.00',
+            quantity=10,
+            food_image='inventory_images/test-image.jpg',
+            is_available=True,
+        )
+
+        response = self.client.get(reverse('inventory_list'))
+
+        self.assertContains(response, '/media/inventory_images/test-image.jpg')
+
+    def test_menu_uses_static_fallback_for_missing_image(self):
+        Inventory.objects.create(
+            item_name='Plain Rice',
+            category='main_course',
+            price='90.00',
+            quantity=5,
+            is_available=True,
+        )
+
+        response = self.client.get(reverse('inventory_list'))
+
+        self.assertContains(response, '/static/inventory_images/momo.webp')
